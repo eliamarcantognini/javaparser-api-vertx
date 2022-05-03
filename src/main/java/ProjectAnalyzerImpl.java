78a1436e08cc70faac1fe95607b4d61b4efa25d9
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-03 19:04:57 +0200

    Add getInterfaceReport implementation in ProjectAnalyzerImpl.java

--- src/main/java/ProjectAnalyzerImpl.java
+++ src/main/java/ProjectAnalyzerImpl.java
@@ -1,10 +1,10 @@
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import reports.ClassReport;
-import reports.InterfaceReport;
-import reports.PackageReport;
-import reports.ProjectReport;
-
+import reports.*;
+import visitors.InterfacesVisitor;
+import java.io.File;
 import java.util.function.Consumer;
 
 public class ProjectAnalyzerImpl implements ProjectAnalyzer {
@@ -18,7 +18,15 @@ public class ProjectAnalyzerImpl implements ProjectAnalyzer {
     @Override
     public Future<InterfaceReport> getInterfaceReport(String srcInterfacePath) {
         return this.vertx.executeBlocking(ev -> {
-
+            try {
+                CompilationUnit cu = StaticJavaParser.parse(new File(srcInterfacePath));
+                InterfacesVisitor interfaceVisitor = new InterfacesVisitor();
+                InterfaceReport interfaceReport = new InterfaceReportImpl();
+                interfaceVisitor.visit(cu,interfaceReport);
+                ev.complete(interfaceReport);
+            } catch (Exception e) {
+                ev.fail("EXEPTION: getInterfaceReport has failed with message: " + e.getMessage());
+            }
         });
     }
 
