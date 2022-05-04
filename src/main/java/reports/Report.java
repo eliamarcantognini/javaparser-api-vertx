ec22bc0dccb2943d48d2d5eb43b56452670917c1
Michele Bachetti <michele.bachetti@studio.unibo.it>
2022-05-04 13:05:03 +0200

    Add Report.java
    Base interface for the reports

---
+++ src/main/java/reports/Report.java
@@ -0,0 +1,8 @@
+package reports;
+
+public interface Report {
+    String getName();
+    void setName(String interfaceName);
+    String getSourceFullPath();
+    void setFullPath(String interfaceFullPath);
+}
