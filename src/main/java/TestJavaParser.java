68bfd714899af8cd50e5dd3fe9d755161c1a4257
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-04 02:14:14 +0200

    Organize code to test futures

--- src/main/java/TestJavaParser.java
+++ src/main/java/TestJavaParser.java
@@ -9,6 +9,10 @@ import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import reports.ClassReport;
+import reports.InterfaceReport;
 
 class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
   public void visit(MethodDeclaration md, List<String> collector) {
@@ -43,16 +47,63 @@ class FullCollector extends VoidVisitorAdapter<Void> {
 
 public class TestJavaParser {
 
-	public static void main(String[] args) throws Exception {
-		CompilationUnit cu = StaticJavaParser.parse(new File("src/main/java/TestJavaParser.java"));
+	// main del prof
+//	public static void main(String[] args) throws Exception {
+		//CompilationUnit cu = StaticJavaParser.parse(new File("src/main/java/TestJavaParser.java"));
 		/*
 		var methodNames = new ArrayList<String>();
 		var methodNameCollector = new MethodNameCollector();
 		methodNameCollector.visit(cu,methodNames);
 		methodNames.forEach(n -> System.out.println("MethodNameCollected:" + n));
 		 */
-		var fullc = new FullCollector();
-		fullc.visit(cu, null);
-		
+		//var fullc = new FullCollector();
+		//fullc.visit(cu, null);
+//	}
+
+	public static void main(String[] args) throws Exception {
+		ProjectAnalyzer projectAnalyzer;
+		projectAnalyzer = new ProjectAnalyzerImpl(Vertx.vertx());
+		testClassReport(projectAnalyzer);
 	}
+
+	private void testInterfaceReport(ProjectAnalyzer projectAnalyzer){
+		Future<InterfaceReport> future = projectAnalyzer.getInterfaceReport("src/main/java/reports/InterfaceReport.java");
+		future.onSuccess(event -> {
+			System.out.println("Prova1");
+			System.out.println(event.toString());
+		});
+		future.onFailure(event ->{
+			System.out.println("Prova2");
+			System.out.println(event.toString());
+		});
+		future.onComplete(event -> {
+			System.out.println("Prova3");
+			System.out.println(event.toString());
+		});
+		System.out.println("Prova4 " + future.result());
+	}
+
+	private static void testClassReport(ProjectAnalyzer projectAnalyzer){
+		Future<ClassReport> future = projectAnalyzer.getClassReport("src/main/java/reports/ClassReportImpl.java");
+		future.onSuccess(event -> {
+			System.out.println("Future Success:");
+			System.out.println(event.toString());
+			System.out.println();
+		});
+		future.onFailure(event ->{
+			System.out.println("Future Failure:");
+			System.out.println(event.toString());
+			System.out.println();
+		});
+		future.onComplete(event -> {
+			System.out.println("Future Complete:");
+			System.out.println(event.toString());
+			System.out.println();
+		});
+		System.out.println("Future Result Without \"on\" construct" + future.result());
+		//future.onSuccess(System.out::println);
+		//future.onFailure(System.out::println);
+		//future.onComplete(System.out::println);
+	}
+
 }
