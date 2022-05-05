c4379f42575f5623602b76ad8192b98a9ff8f39c
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-05 10:54:52 +0200

    Add Javadoc

--- src/main/java/reports/info/interfaces/FieldInfo.java
+++ src/main/java/reports/info/interfaces/FieldInfo.java
@@ -5,8 +5,32 @@ import reports.interfaces.Report;
 import java.util.Optional;
 
 public interface FieldInfo {
-	String getFieldName();
-	String getFieldType();
-	Optional<String> getFieldModifiers();
-	Report getParentClassReport();
+    
+    /**
+     * Get the name of the field
+     *
+     * @return the name
+     */
+    String getFieldName();
+
+    /**
+     * Get the type of the field
+     *
+     * @return the type name
+     */
+    String getFieldType();
+
+    /**
+     * Get all the modifiers of the method, if present
+     *
+     * @return an {@link Optional<String>} if the modifiers are present, Empty otherwise
+     */
+    Optional<String> getFieldModifiers();
+
+    /**
+     * Get the full parent {@link reports.interfaces.ClassReport}
+     *
+     * @return the parent report
+     */
+    Report getParentClassReport();
 }
