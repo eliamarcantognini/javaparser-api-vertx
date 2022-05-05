f826403fa77e8bb3f964952b5e290a3f2abe9cbd
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-05 10:47:37 +0200

    Add Javadoc

--- src/main/java/reports/interfaces/ClassReport.java
+++ src/main/java/reports/interfaces/ClassReport.java
@@ -6,7 +6,17 @@ import java.util.List;
 
 public interface ClassReport extends InterfaceReport {
 
+    /**
+     * Get a list of fields inside the class
+     *
+     * @return a {@link List} of {@link FieldInfo}
+     */
     List<FieldInfo> getFieldsInfo();
 
+    /**
+     * Add a {@link FieldInfo} to the report
+     *
+     * @param fieldInfo the {@link FieldInfo}
+     */
     void addFieldInfo(FieldInfo fieldInfo);
 }
