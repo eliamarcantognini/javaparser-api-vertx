5cb6d30ed9d3976bc60deec26e8c0132cf8e8962
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-05 10:30:35 +0200

    Reformat

--- src/main/java/reports/interfaces/ClassReport.java
+++ src/main/java/reports/interfaces/ClassReport.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ClassReport extends InterfaceReport {
 
-	List<FieldInfo> getFieldsInfo();
+    List<FieldInfo> getFieldsInfo();
 
-	void addFieldInfo(FieldInfo fieldInfo);
+    void addFieldInfo(FieldInfo fieldInfo);
 }
