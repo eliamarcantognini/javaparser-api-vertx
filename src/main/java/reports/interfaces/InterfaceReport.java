04aa74874b37050440549802ba7b814cece8add9
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-05 10:45:30 +0200

    Add Javadoc

--- src/main/java/reports/interfaces/InterfaceReport.java
+++ src/main/java/reports/interfaces/InterfaceReport.java
@@ -4,8 +4,19 @@ import reports.info.interfaces.MethodInfo;
 
 import java.util.List;
 
-public interface InterfaceReport extends Report{
+public interface InterfaceReport extends Report {
 
+    /**
+     * Get the list of the methods inside the item (class/interface)
+     *
+     * @return a {@link List} of {@link MethodInfo}
+     */
     List<MethodInfo> getMethodsInfo();
+
+    /**
+     * Add a {@link MethodInfo}
+     *
+     * @param methodInfo the {@link MethodInfo}
+     */
     void addMethodInfo(MethodInfo methodInfo);
 }
