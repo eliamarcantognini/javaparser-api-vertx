5b7adec1a9d9c700b82c0ced8d9897286c9a1d18
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-06 11:03:12 +0200

    Add FileChooser behaviour to listener

--- src/main/java/view/ViewListener.java
+++ src/main/java/view/ViewListener.java
@@ -1,22 +1,53 @@
 package view;
 
-public class ViewListener {
-//    private final Simulator simulator;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-//    public ViewListener(final Simulator simulator) {
-//        this.simulator = simulator;
+public class ViewListener{
+//    private final Controller controller;
+
+    private final static String TITLE = "SELECT JAVA FILE";
+
+//    public ViewListener(final Controller controller) {
+//        this.controller = controller;
 //    }
 
     public void eventPerformed(Commands code) {
         switch (code) {
             case START:
-//                simulator.startSimulation();
+//                controller.startParsing();
                 break;
             case STOP:
-//                simulator.stopSimulation();
+//                controller.stopParsing();
                 break;
+            case FOLDER:
+                String path = getFilePath();
             default:
                 break;
         }
+    }
+
+    private String getFilePath(){
+        JFileChooser chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new java.io.File("."));
+        chooser.setDialogTitle(TITLE);
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setFileFilter(new FileNameExtensionFilter("Java File", "java"));
+        //
+        // disable the "All files" option.
+        //
+        chooser.setAcceptAllFileFilterUsed(false);
+        //
+        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+            System.out.println("getCurrentDirectory(): "
+                    +  chooser.getCurrentDirectory());
+            System.out.println("getSelectedFile() : "
+                    +  chooser.getSelectedFile());
+            return chooser.getSelectedFile().toString();
+        }
+        else {
+            System.out.println("No Selection ");
+        }
+        return "";
     }
 }
