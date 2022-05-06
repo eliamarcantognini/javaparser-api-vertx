d03b62de5cdbe96d6a57a6defccc875111b1b9bf
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-06 18:48:40 +0200

    Add ProjectDTO

---
+++ src/main/java/dto/ProjectDTO.java
@@ -0,0 +1,14 @@
+package dto;
+
+import java.util.List;
+
+public record ProjectDTO(ClassInterfaceDTO mainClass, List<PackageDTO> packages) {
+    ClassInterfaceDTO getClass(String name) {
+        final ClassInterfaceDTO[] classDTO = new ClassInterfaceDTO[1];
+        classDTO[0] = null;
+        this.packages.forEach(p -> p.classes().forEach(c -> {
+            if (c.name().equals(name)) classDTO[0] = c;
+        }));
+        return classDTO[0];
+    }
+}
