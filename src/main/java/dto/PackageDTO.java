d6b0eab723280672fc44bb547588534d6f496db4
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-06 18:22:25 +0200

    Add PackageDTO

---
+++ src/main/java/dto/PackageDTO.java
@@ -0,0 +1,6 @@
+package dto;
+
+import java.util.List;
+
+public record PackageDTO(String name, String path, List<ClassInterfaceDTO> classes,
+                         List<ClassInterfaceDTO> interfaces) {}
