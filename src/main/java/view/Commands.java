e0833dac31dc00af184d9030605d24d7eb375ba6
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-09 16:48:41 +0200

    Add javadoc to view/Commands.java

--- src/main/java/view/Commands.java
+++ src/main/java/view/Commands.java
@@ -1,7 +1,22 @@
 package view;
 
+/**
+ * Commands use to manage analyzing of a project.
+ *
+ * @see ViewListener
+ * @see GUIView
+ */
 public enum Commands {
+    /**
+     * Command to start analyze project.
+     */
     START,
+    /**
+     * Command to stop analyze project.
+     */
     STOP,
+    /**
+     * Command to open filesystem view.
+     */
     FOLDER
 }
