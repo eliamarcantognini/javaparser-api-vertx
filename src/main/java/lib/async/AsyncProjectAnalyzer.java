0b7c2671a44d75f77ae481a34682e91e2a8f5019
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-10 19:16:20 +0200

    Reformat code

--- src/main/java/lib/async/AsyncProjectAnalyzer.java
+++ src/main/java/lib/async/AsyncProjectAnalyzer.java
@@ -2,7 +2,9 @@ package lib.async;
 
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
-import io.vertx.core.*;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 import io.vertx.core.impl.future.PromiseImpl;
 import lib.ProjectAnalyzer;
 import lib.reports.ClassReportImpl;
@@ -15,12 +17,9 @@ import lib.visitors.InterfacesVisitor;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Stream;
 
 public class AsyncProjectAnalyzer implements ProjectAnalyzer {
 
@@ -64,7 +63,7 @@ public class AsyncProjectAnalyzer implements ProjectAnalyzer {
         PackageReport packageReport = new PackageReportImpl();
         Promise<PackageReport> promise = new PromiseImpl<>();
         PackageVerticle vert = new PackageVerticle(this, promise, srcPackagePath);
-        Future<String> verticleID =  this.vertx.deployVerticle(vert);
+        Future<String> verticleID = this.vertx.deployVerticle(vert);
 
         return promise.future();
     }
@@ -76,7 +75,7 @@ public class AsyncProjectAnalyzer implements ProjectAnalyzer {
         ProjectReport packageReport = new ProjectReportImpl();
         Promise<ProjectReport> promise = new PromiseImpl<>();
         ProjectVerticle vert = new ProjectVerticle(this, promise, srcProjectFolderPath);
-        Future<String> verticleID =  this.vertx.deployVerticle(vert);
+        Future<String> verticleID = this.vertx.deployVerticle(vert);
 
         return promise.future();
     }
@@ -90,11 +89,6 @@ public class AsyncProjectAnalyzer implements ProjectAnalyzer {
         return StaticJavaParser.parse(new File(path));
     }
 
-    private void checkCompletion(AtomicInteger completed, List<Future<ClassReport>> classReports, List<Future<InterfaceReport>> interfaceReports, Promise<PackageReport> ev, PackageReport packageReport) {
-        completed.getAndIncrement();
-        if (completed.get() == classReports.size() + interfaceReports.size()) ev.complete(packageReport);
-    }
-
     void setPackageNameAndPath(PackageReport packageReport, AtomicBoolean set, String name, String sourceFullPath, Report res) {
         if (!set.get()) {
             var s = sourceFullPath.split("\\.");
@@ -102,6 +96,11 @@ public class AsyncProjectAnalyzer implements ProjectAnalyzer {
             packageReport.setFullPath(sourceFullPath.substring(0, sourceFullPath.length() - name.length() - 1));
             set.set(true);
         }
+    }
+
+    private void checkCompletion(AtomicInteger completed, List<Future<ClassReport>> classReports, List<Future<InterfaceReport>> interfaceReports, Promise<PackageReport> ev, PackageReport packageReport) {
+        completed.getAndIncrement();
+        if (completed.get() == classReports.size() + interfaceReports.size()) ev.complete(packageReport);
     }
 
 }
