bf430832709b93c47da1cd354ea50348de95fb8d
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-11 19:12:30 +0200

    Create StartGUI

---
+++ src/main/java/view/StartGUI.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class StartGUI extends JFrame {
+
+    public StartGUI(int w, int h){
+        setTitle("JavaParser Analyzer");
+        setSize(w, h);
+    }
+
+}
