e115a987948318ea456cd8ee05dd8eec2483a99a
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-11 18:08:36 +0200

    Implement ProjectReport

--- src/main/java/lib/reports/ProjectReportImpl.java
+++ src/main/java/lib/reports/ProjectReportImpl.java
@@ -4,21 +4,50 @@ import lib.reports.interfaces.ClassReport;
 import lib.reports.interfaces.PackageReport;
 import lib.reports.interfaces.ProjectReport;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class ProjectReportImpl implements ProjectReport {
-    @Override
-    public ClassReport getMainClass() {
-        return null;
+
+    private final List<PackageReport> packageReports;
+    private ClassReport mainClass;
+
+    public ProjectReportImpl() {
+        this.packageReports = new LinkedList<>();
     }
 
     @Override
-    public List<PackageReport> getAllProjects() {
-        return null;
+    public ClassReport getMainClass() {
+        return this.mainClass;
+    }
+
+    @Override
+    public void setMainClass(ClassReport classReport) {
+        this.mainClass = classReport;
+    }
+
+    @Override
+    public List<PackageReport> getAllPackageReports() {
+        return this.packageReports;
+    }
+
+    @Override
+    public void addPackageReport(PackageReport packageReport) {
+        this.packageReports.add(packageReport);
     }
 
     @Override
     public ClassReport getClassReport(String fullClassName) {
-        return null;
+        final ClassReport[] classReport = new ClassReportImpl[1];
+        classReport[0] = null;
+        this.packageReports.forEach(p -> p.getClassesReports().forEach(c -> {
+            if (c.getName().equals(fullClassName)) classReport[0] = c;
+        }));
+        return classReport[0];
+    }
+
+    @Override
+    public String toString() {
+        return "Project Report:\n" + "\tProject Reports: \n" + this.packageReports + "\n";
     }
 }
