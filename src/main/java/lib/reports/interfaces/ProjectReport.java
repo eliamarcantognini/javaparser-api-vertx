83bcaef8f4889402d36500613dd1ed052698d383
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-11 18:04:08 +0200

    Refactor ProjectReport and add method setMainClass

--- src/main/java/lib/reports/interfaces/ProjectReport.java
+++ src/main/java/lib/reports/interfaces/ProjectReport.java
@@ -5,8 +5,12 @@ import java.util.List;
 public interface ProjectReport {
 
 	ClassReport getMainClass();
+
+	void setMainClass(ClassReport classReport);
 	
-	List<PackageReport> getAllProjects();
+	List<PackageReport> getAllPackageReports();
+
+	void addPackageReport (PackageReport packageReport);
 	
 	ClassReport getClassReport(String fullClassName);
 }
