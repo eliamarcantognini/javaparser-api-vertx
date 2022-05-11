cfe2d4e65f52782981ca020c731694f4118dd3e6
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-11 22:43:58 +0200

    Add Strings

---
+++ src/main/java/view/Strings.java
@@ -0,0 +1,11 @@
+package view;
+
+public final class Strings {
+    public final static String CHOOSER = "SELECT PROJECT FOLDER";
+    public final static String SELECT_PROJECT = "SELECT PROJECT";
+    public final static String START = "START";
+    public final static String STOP = "STOP";
+    public static final String SAVE = "SAVE";
+    public final static String TITLE = "JavaParser Analyzer GUI";
+    public final static String OUTPUT_PATH = "./output.json";
+}
