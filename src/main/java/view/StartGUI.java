67101bcf198f6f500f21135a7a0b642076ddbb7d
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-11 22:44:17 +0200

    Implement StartGUI

--- src/main/java/view/StartGUI.java
+++ src/main/java/view/StartGUI.java
@@ -1,12 +1,20 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class StartGUI extends JFrame {
 
-    public StartGUI(int w, int h){
-        setTitle("JavaParser Analyzer");
+    public StartGUI(ViewListener listener){
+        var w = Toolkit.getDefaultToolkit().getScreenSize().width / 5;
+        var h = Toolkit.getDefaultToolkit().getScreenSize().height / 5;
+        setTitle(Strings.TITLE);
         setSize(w, h);
+        setResizable(true);
+        var btn = new JButton(Strings.SELECT_PROJECT);
+        btn.addActionListener(e -> listener.eventPerformed(Commands.ANALYZE));
+        add(btn);
+        setVisible(true);
     }
 
 }
