b2ab7c576a7dc01fe8befa564939070dc6434328
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-13 02:45:18 +0200

    Create App.java to launch application

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+import controller.AnalysisController;
+import view.AnalyzerGUI;
+import view.StartGUI;
+import view.ViewListener;
+
+public class App {
+    public static void main(String[] args) {
+//        AnalysisController analysisController = new AnalysisController();
+//        ViewListener viewListener = new ViewListener(analysisController);
+//        viewListener.setViewToRunForAnalysis(new AnalyzerGUI(viewListener));
+//        new StartGUI(viewListener);
+        // TODO: Can be done in one line
+    }
+}
