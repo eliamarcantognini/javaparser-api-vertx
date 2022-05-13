474e04eb25d82438117c10cec717ae8e03b69560
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-13 10:45:57 +0200

    Fix PathChooser to take projects everywhere

--- src/main/java/view/GUI/PathChooserGUI.java
+++ src/main/java/view/GUI/PathChooserGUI.java
@@ -14,12 +14,8 @@ public class PathChooserGUI {
         chooser.setDialogTitle(Strings.CHOOSER);
         chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
         var choice = chooser.showOpenDialog(null);
-        if (choice == JFileChooser.APPROVE_OPTION) {
-            // Delete the "." at the end of the path
-            var _f = f.getAbsolutePath().substring(0, f.getAbsolutePath().length() - 1);
-            // Transform the absolute path in a relative path
-            return chooser.getSelectedFile().getAbsolutePath().substring(_f.length());
-        }
+        if (choice == JFileChooser.APPROVE_OPTION)
+            return chooser.getSelectedFile().getAbsolutePath();
         return "";
     }
 }
