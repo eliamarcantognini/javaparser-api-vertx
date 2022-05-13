62f3ef74d6251cb120221cb1c9d05285d18c161e
Elia Marcantognini <elia.marcantognini@gmail.com>
2022-05-13 09:28:55 +0200

    Add two strings

--- src/main/java/view/Strings.java
+++ src/main/java/view/Strings.java
@@ -6,6 +6,8 @@ public final class Strings {
     public final static String START = "START";
     public final static String STOP = "STOP";
     public static final String SAVE = "SAVE";
+    public static final String VALID_PATH = "Select a valid path";
+    public static final String PATH_ERROR = "PATH ERROR";
     public final static String TITLE = "JavaParser Analyzer GUI";
     public final static String OUTPUT_PATH = "./output.json";
 }
