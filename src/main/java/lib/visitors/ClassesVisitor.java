8dfd9eff6013ee40171aa008c88b572d81238ceb
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-14 04:37:41 +0200

    Add javadoc to ClassesVisitor.java and delete unused test methods

--- src/main/java/lib/visitors/ClassesVisitor.java
+++ src/main/java/lib/visitors/ClassesVisitor.java
@@ -1,14 +1,23 @@
 package lib.visitors;
 
 import com.github.javaparser.ast.body.FieldDeclaration;
-import com.github.javaparser.ast.body.MethodDeclaration;
 import lib.Logger;
 import lib.reports.info.interfaces.FieldInfo;
 import lib.reports.interfaces.ClassReport;
 import lib.reports.info.InfoBuilder;
 
+/**
+ * Visitor for classes that extends from {@link FileVisitor}
+ *
+ * @see com.github.javaparser.ast.visitor
+ */
 public class ClassesVisitor extends FileVisitor<ClassReport> {
 
+    /**
+     * Class constructor
+     *
+     * @param logger logger where send message when incurred new find
+     */
     public ClassesVisitor(Logger logger) {
         super(true, logger);
     }
@@ -16,7 +25,6 @@ public class ClassesVisitor extends FileVisitor<ClassReport> {
     public void visit(FieldDeclaration fd, ClassReport collector) {
         super.visit(fd, collector);
         InfoBuilder builder = new InfoBuilder()
-                //TODO: hardcoded fd.getVariable(0) per prendere il nome del campo
                 .name(""+fd.getVariable(0))
                 .type(""+fd.getElementType())
                 .modifiers(""+fd.getModifiers())
@@ -24,40 +32,5 @@ public class ClassesVisitor extends FileVisitor<ClassReport> {
         FieldInfo field = builder.buildField();
         logger.log(field);
         collector.addFieldInfo(field);
-//        this.testFieldDeclarationMethods(fd);
-    }
-
-    private void testMethodDeclarationMethods(MethodDeclaration md){
-        System.out.println("=== START VISIT METHODS OF METHOD DECLARATION ===");
-        //System.out.println("md.getDeclarationAsString() -> " + md.getDeclarationAsString());
-        System.out.println("md.getNameAsExpression() -> " + md.getNameAsExpression());
-        //System.out.println("md.getParameters() -> " + md.getParameters());
-        System.out.println("md.getModifiers() -> " + md.getModifiers());
-        System.out.println("md.getRange() -> " + md.getRange());
-        System.out.println("md.getRange().get().begin -> " + md.getRange().get().begin);
-        System.out.println("md.getRange().get().begin.line -> " + md.getRange().get().begin.line);
-        System.out.println("md.getRange().get().begin.column -> " + md.getRange().get().begin.column);
-        System.out.println("md.getRange().get().end -> " + md.getRange().get().end);
-        System.out.println("md.getRange().get().end.line -> " + md.getRange().get().end.line);
-        System.out.println("md.getRange().get().end.column -> " + md.getRange().get().end.column);
-        System.out.println("md.getRange().get().getLineCount() -> " + md.getRange().get().getLineCount());
-        //System.out.println("md.getTokenRange() -> " + md.getTokenRange());
-        //System.out.println("md.getTokenRange().get() -> " + md.getTokenRange().get());
-        //System.out.println("md.getTokenRange().get().getBegin() -> " + md.getTokenRange().get().getBegin());
-        //System.out.println("md.getTokenRange().get().getEnd() -> " + md.getTokenRange().get().getEnd());
-        System.out.println("=== END VISIT METHODS OF METHOD DECLARATION ===");
-    }
-
-    private void testFieldDeclarationMethods(FieldDeclaration fd){
-        System.out.println("=== START VISIT METHODS OF FIELD DECLARATION ===");
-        System.out.println("fd => " + fd);
-        System.out.println("fd.asFieldDeclaration() => " + fd.asFieldDeclaration());
-        System.out.println("fd.getModifiers() => " + fd.getModifiers());
-        System.out.println("fd.getVariables() => " + fd.getVariables());
-        System.out.println("fd.getRange() => " + fd.getRange());
-        System.out.println("fd.getBegin() => " + fd.getBegin());
-        System.out.println("fd.getElementType() => " + fd.getElementType());
-//        System.out.println("fd => " + fd);
-        System.out.println("=== END VISIT METHODS OF FIELD DECLARATION ===");
     }
 }
