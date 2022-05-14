1c779eb506f07592d49bdfa94f4e394b94a3bdd9
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-05-14 04:40:36 +0200

    Add javadoc to InterfacesVisitor.java

--- src/main/java/lib/visitors/InterfacesVisitor.java
+++ src/main/java/lib/visitors/InterfacesVisitor.java
@@ -1,38 +1,21 @@
 package lib.visitors;
 
-import com.github.javaparser.ast.body.MethodDeclaration;
 import lib.Logger;
 import lib.reports.interfaces.InterfaceReport;
 
+/**
+ * Visitor for interfaces that extends from {@link FileVisitor}
+ *
+ * @see com.github.javaparser.ast.visitor
+ */
 public class InterfacesVisitor extends FileVisitor<InterfaceReport> {
 
+    /**
+     * Class constructor
+     *
+     * @param logger logger where send message when incurred new find
+     */
     public InterfacesVisitor(Logger logger) {
         super(false, logger);
     }
-
-    private void testMethodDeclarationMethods(MethodDeclaration md){
-        System.out.println("=== START VISIT METHOD ===");
-        //System.out.println("md.getDeclarationAsString() -> " + md.getDeclarationAsString());
-        System.out.println("md.getNameAsExpression() -> " + md.getNameAsExpression());
-        //System.out.println("md.getParameters() -> " + md.getParameters());
-        System.out.println("md.getModifiers() -> " + md.getModifiers());
-        System.out.println("md.getRange() -> " + md.getRange());
-        System.out.println("md.getRange().get().begin -> " + md.getRange().get().begin);
-        System.out.println("md.getRange().get().begin.line -> " + md.getRange().get().begin.line);
-        System.out.println("md.getRange().get().begin.column -> " + md.getRange().get().begin.column);
-        System.out.println("md.getRange().get().end -> " + md.getRange().get().end);
-        System.out.println("md.getRange().get().end.line -> " + md.getRange().get().end.line);
-        System.out.println("md.getRange().get().end.column -> " + md.getRange().get().end.column);
-        System.out.println("md.getRange().get().getLineCount() -> " + md.getRange().get().getLineCount());
-        //System.out.println("md.getTokenRange() -> " + md.getTokenRange());
-        //System.out.println("md.getTokenRange().get() -> " + md.getTokenRange().get());
-        //System.out.println("md.getTokenRange().get().getBegin() -> " + md.getTokenRange().get().getBegin());
-        //System.out.println("md.getTokenRange().get().getEnd() -> " + md.getTokenRange().get().getEnd());
-        System.out.println("=== END VISIT METHOD ===");
-    }
-
-//    public void visit(PackageDeclaration pd, InterfaceReport collector) {
-//        super.visit(pd, collector);
-//        collector.setInterfaceFullPath(pd.getNameAsString());
-//    }
 }
